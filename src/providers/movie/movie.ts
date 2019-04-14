18c77e2d214c8886e926f51391b1d311367092d9
brunoalvesds <brunoalves.ds95@gmail.com>
2019-04-14 20:29:48 -0300

    Consumo de API e app funcionando completo na pagina FILMES

--- src/providers/movie/movie.ts
+++ src/providers/movie/movie.ts
@@ -12,21 +12,20 @@ export class MovieProvider {
   private baseApiPath = 'https://api.themoviedb.org/3';
 
   constructor(public http: HttpClient) {
-    console.log('Hello MovieProvider Provider');
+    console.log('Testando o Provider');
   }
 
   getApiKey(): string{
     return '10e83380c28f364ddd801327517a617e';
+  }
+  
+  getPopularMovies() {
+    return this.http.get(this.baseApiPath + '/movie/popular?api_key='
+    + this.getApiKey());
   }
 
   getLatestMovies() {
     return this.http.get(this.baseApiPath + '/movie/latest?api_key='
     + this.getApiKey());
   }
-
-  getPopularMovies() {
-    return this.http.get(this.baseApiPath + '/movie/popular?api_key='
-    + this.getApiKey());
-  }
-
 }
